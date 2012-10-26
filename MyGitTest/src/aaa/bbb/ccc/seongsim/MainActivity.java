44fdca380dc8b5724a42124b94647f4409ed6f74
tocaya <tocaya@ismaker.com>
2012-10-26 17:58:30 +0900

    rtrtr

--- MyGitTest/src/aaa/bbb/ccc/seongsim/MainActivity.java
+++ MyGitTest/src/aaa/bbb/ccc/seongsim/MainActivity.java
@@ -6,14 +6,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity { 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-
+ 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
