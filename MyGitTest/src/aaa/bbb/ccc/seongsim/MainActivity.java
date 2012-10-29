effa468f7e605648c5be1128865761a06024e4b8
tocaya <tocaya@ismaker.com>
2012-10-29 18:19:39 +0900

    555

--- MyGitTest/src/aaa/bbb/ccc/seongsim/MainActivity.java
+++ MyGitTest/src/aaa/bbb/ccc/seongsim/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
-public class MainActivity extends Activity { 
+public class MainActivity extends Activity {  
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
