c94f2062560e89eb96feb30f3bdf29020823df47
tocaya <tocaya@ismaker.com>
2012-10-29 18:55:24 +0900

    777

--- MyGitTest/src/aaa/bbb/ccc/seongsim/MainActivity.java
+++ MyGitTest/src/aaa/bbb/ccc/seongsim/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {  
 
-    @Override
+    @Override 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
